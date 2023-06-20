3a6307801e47e53af19eb72a07940ffe5913f5b0
matildeopbravo <matildeopbravo@gmail.com>
2023-06-20 17:12:42 +0100

    Implemented logout

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var userController = require('../controllers/user')
-var userModel = require('../models/user')
-var passport = require('passport')
-var jwt = require('jsonwebtoken')
-var auth = require('../shared/auth')
+var userController = require("../controllers/user");
+var userModel = require("../models/user");
+var passport = require("passport");
+var jwt = require("jsonwebtoken");
+var auth = require("../shared/auth");
 
-router.get('/', auth.isLogged, function(req, res, next) {
-  res.render('index', {title: 'Express'})
+router.get("/", auth.isLogged, function (req, res, next) {
+  res.render("index", { title: "LearnVault" });
 });
 
-router.get('/', function(req, res, next) {
-  res.redirect('/login')
+router.get("/", function (req, res, next) {
+  res.redirect("/login");
+});
+
+router.get("/logout", auth.isLogged, function (req, res, next) {
+  req.session.destroy(function () {
+    res.clearCookie("access_token_learnvault");
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
