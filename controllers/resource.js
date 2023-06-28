047f0f42229a4e2e38a1920c787b3778d8937012
matildeopbravo <matildeopbravo@gmail.com>
2023-06-28 12:13:11 +0100

    Added missing curly brackets to aggregate query in resource controller

--- controllers/resource.js
+++ controllers/resource.js
@@ -1,23 +1,26 @@
 var resourceModel = require("../models/resource");
 
 module.exports.list = () => {
-  return resourceModel.aggregate([{
-    $lookup: {
-      from: 'users',
-      localField: 'posterID',
-      foreignField: '_id',
-      as: 'user'
+  return resourceModel.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "posterID",
+        foreignField: "_id",
+        as: "user",
+      },
     },
-    $unwind: '$user'
-  }
+    {
+      $unwind: "$user",
+    },
   ]);
 };
 
 module.exports.insert = async (resource) => {
-  return await resourceModel.create(resource)
+  return await resourceModel.create(resource);
 };
 
 module.exports.isPoster = async (resourceID, userID) => {
-  let {posterID} = await resourceModel.findOne({_id: resourceID})
-  return posterID == userID
-}
+  let { posterID } = await resourceModel.findOne({ _id: resourceID });
+  return posterID == userID;
+};
