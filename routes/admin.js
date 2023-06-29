f1a8cefea718ca497725824e4aa73b89138314df
Pedro Alves <pta2002@pta2002.com>
2023-06-29 23:23:43 +0100

    manage resources

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 let router = require("express").Router();
 let userController = require("../controllers/user")
+let resourceController = require("../controllers/resource")
 let auth = require('../shared/auth')
 
 router.get("/", auth.isAdmin, async (req, res) => {
-  users = await userController.getAll({ page: 0, limit: 20 })
+  let users = await userController.getAll({ page: 0, limit: 20 })
+  let resources = await resourceController.list()
 
-  res.render('admin', { users })
+  res.render('admin', { users, resources })
 })
 
 module.exports = router;
